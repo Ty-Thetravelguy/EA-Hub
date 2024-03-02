21acea8a357ef40090c700a4db84bfd7c537bef2
Tyrone <145229174+Ty-Thetravelguy@users.noreply.github.com>
2024-03-02 11:03:06 +0000

    finished selelct travel type refund funcationality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ var consultantEmail;
 var travellerName;
 var travelTypeRefund;
 var dateOfTravel;
-var refundType;
+var refundTypeAir;
+var refundDomOrIntRail;
 var reasonForVoidAir;
 var reasonForRefundAir;
 var reasonForRefund;
@@ -21,72 +22,157 @@ var anyThingElseToKnow;
 
 
 document.getElementById('travelTypeRefund').addEventListener('change', function () {
-    const refundTypeFields = document.getElementById('refundType'); // For 'Flight' specific fields
-    const domOrIntRail = document.getElementById('domOrIntRail'); // For 'Rail' specific fields
-    const supplierFields = document.getElementById('supplier'); // For all other types
-    refundTypeFields.innerHTML = ''; // Clear previous 'Flight' content
-    domOrIntRail.innerHTML = ''; // Clear previous 'Rail' content
-    supplierFields.innerHTML = ''; // Clear previous 'Supplier' content
+    const refundTypeField = document.getElementById('refundTypeAir');
+    const domOrIntRailField = document.getElementById('refundDomOrIntRail');
+    const reasonForRefundField = document.getElementById('reasonForRefund');
+    const supplierAccomField = document.getElementById('supplierAccom');
+    const supplierCarTaxiField = document.getElementById('supplierCarTaxi');
+    const supplierAncField = document.getElementById('supplierAnc');
+
+    refundTypeField.innerHTML = '';
+    domOrIntRailField.innerHTML = '';
+    reasonForRefundField.innerHTML = '';
+    supplierAccomField.innerHTML = '';
+    supplierCarTaxiField.innerHTML = '';
+    supplierAncField.innerHTML = '';
 
     if (this.value === 'Flight') {
-        const htmlContent = `
+        refundTypeField.innerHTML = `
+        <div>
+            <label class="form-group" for="flightRefundType">Select refund type for Flight:</label>
+            <select class="form-control" id="flightRefundType" required>
+                <option value="" selected disabled>Please Select</option>
+                <option value="Void">Void</option>
+                <option value="Full">Full</option>
+                <option value="Partial">Partial</option>
+                <option value="Tax only">Tax only</option>
+            </select>
+        </div>`;
+
+        // Use setTimeout to ensure the DOM has updated
+        setTimeout(() => {
+            document.getElementById('flightRefundType').addEventListener('change', function () {
+                const reasonForVoidAir = document.getElementById('reasonForVoidAir');
+                const reasonForRefundAir = document.getElementById('reasonForRefundAir');
+
+                reasonForVoidAir.innerHTML = ''; // Reset content
+                reasonForRefundAir.innerHTML = ''; // Reset content
+
+                if (this.value === 'Void') {
+                    reasonForVoidAir.innerHTML = `
+                        <div>
+                            <label class="form-group" for="reasonForVoidAir">Reason for Void:</label>
+                            <select class="form-control" id="reasonForVoidAir" required>
+                                <option value="" selected disabled>Please Select</option>
+                                <option value="Agent Error">Agent Error</option>
+                                <option value="Client Error">Client Error</option>
+                                <option value="Agent request – Higher profit booking">Agent request – Higher profit booking</option>
+                            </select>
+                        </div>`;
+                } else {
+                    reasonForRefundAir.innerHTML = `
+                        <div>
+                            <label class="form-group" for="reasonForRefundAir">Reason for Refund:</label>
+                            <select class="form-control" id="reasonForRefundAir" required>
+                                <option value="" selected disabled>Please Select</option>
+                                <option value="Client's request - Change of plans">Client's request - Change of plans</option>
+                                <option value="Agent Error">Agent Error</option>
+                                <option value="Involuntary Change">Involuntary Change</option>
+                                <option value="Official government travel advice">Official government travel advice</option>
+                                <option value="Supplier issue">Supplier issue</option>
+                            </select>
+                        </div>`;
+                }
+            });
+        }, 0);
+    } else if (this.value === 'Accommodation') {
+        reasonForRefundField.innerHTML = `
             <div>
-                <label for="flightRefundType">Select refund type for Flight:</label>
-                <select id="flightRefundType" required>
+                <label class="form-group" for="reasonForRefund">Reason for Refund:</label>
+                <select class="form-control" id="reasonForRefund" required>
                     <option value="" selected disabled>Please Select</option>
-                    <option value="Void">Void</option>
-                    <option value="Full">Full</option>
-                    <option value="Partial">Partial</option>
-                    <option value="Tax only">Tax only</option>
-                </select>
-            </div>
-        `;
-        refundTypeFields.innerHTML = htmlContent; // Insert into 'Flight' specific container
-    } else if (this.value === 'Rail') {
-        const htmlContent = `
-            <div>
-                <label for="railRefundType">Select if this is a Domestic or International Rail Refund:</label>
-                <select id="railRefundType" required>
-                    <option value="" selected disabled>Please Select</option>
-                    <option value="Domestic Rail Refund">Domestic Rail Refund</option>
-                    <option value="International Rail Refund">International Rail Refund</option>
+                    <option value="Client's request - Change of plans">Client's request - Change of plans</option>
+                    <option value="Agent Error">Agent Error</option>
+                    <option value="Client Error">Client Error</option>
+                    <option value="Official government travel advice">Official government travel advice</option>
+                    <option value="Supplier issue">Supplier issue</option>
                 </select>
             </div>`;
-        domOrIntRail.innerHTML = htmlContent; // Insert into 'Rail' specific container
-    } else {
-        let htmlContent = `<div><label for="${this.value}Supplier">Select the Supplier:</label>`;
-        // Begin supplier-specific select or input
-        htmlContent += `<select id="${this.value}SupplierSelect" required>
-                            <option value="" selected disabled>Please Select</option>`;
-        // Populate options dynamically based on selection
-        switch (this.value) {
-            case 'Accommodation':
-                htmlContent += `
-                    <option value="GDS Hotel">GDS Hotel</option>
-                    <option value="Expedia">Expedia</option>
-                    <option value="Booking.com">Booking.com</option>
-                    <option value="Hotel Direct">Hotel Direct</option>`;
-                break;
-            case 'Car Hire & Taxi':
-                htmlContent += `
-                    <option value="Avis">Avis</option>
-                    <option value="Enterprise">Enterprise</option>
-                    <option value="Europcar">Europcar</option>
-                    <option value="Hertz">Hertz</option>
-                    <option value="Sixt">Sixt</option>
-                    <option value="Blacklane">Blacklane</option>
-                    <option value="Transferz">Transferz</option>
-                    <option value="Jyrney">Jyrney</option>
-                    <option value="Other">Other</option>`;
-                break;
-            case 'Ancillary':
-                // Switch to input for Ancillary as there's no predefined list
-                htmlContent = `<div><label for="ancillarySupplier">Name of Supplier:</label>
-                                <input type="text" id="ancillarySupplier" placeholder="Name of Supplier" required></div>`;
-                break;
-        }
-        htmlContent += `</select></div>`;
-        supplierFields.innerHTML = htmlContent; // Insert into general 'Supplier' container only if not Ancillary
-    }
-});
 
+        supplierAccomField.innerHTML = `
+        <div>
+            <label class="form-group" for="supplierAccom">Select the Accommodation Supplier</label>
+            <select class="form-control" id="supplierAccom" required>
+                <option value="" selected disabled>Please Select</option>
+                <option value="GDS Hotel">GDS Hotel</option>
+                <option value="Expedia">Expedia</option>
+                <option value="Booking.com">Booking.com</option>
+                <option value="Hotel Direct">Hotel Direct</option>
+            </select>
+        </div>
+        `
+    } else if (this.value === 'Car Hire & Taxi') {
+        reasonForRefundField.innerHTML = `
+            <div>
+                <label class="form-group" for="reasonForRefund">Reason for Refund:</label>
+                <select class="form-control" id="reasonForRefund" required>
+                    <option value="" selected disabled>Please Select</option>
+                    <option value="Client's request - Change of plans">Client's request - Change of plans</option>
+                    <option value="Agent Error">Agent Error</option>
+                    <option value="Client Error">Client Error</option>
+                    <option value="Official government travel advice">Official government travel advice</option>
+                    <option value="Supplier issue">Supplier issue</option>
+                </select>
+            </div>`;
+
+        supplierCarTaxiField.innerHTML = `
+        <div>
+            <label class="form-group" for="supplierCarTaxi">Select the Accommodation Supplier</label>
+            <select class="form-control" id="supplierCarTaxi" required>
+                <option value="" selected disabled>Please Select</option>
+                <option value="Avis">Avis</option>
+                <option value="Enterprise">Enterprise</option>
+                <option value="Europcar">Europcar</option>
+                <option value="Hertz">Hertz</option>
+                <option value="Sixt">Sixt</option>
+                <option value="Blacklane">Blacklane</option>
+                <option value="Transferz">Transferz</option>
+                <option value="Jyrney">Jyrney</option>
+                <option value="Other">Other</option>
+            </select>
+        </div>
+        `
+    } else if (this.value === 'Rail') {
+        domOrIntRailField.innerHTML = `
+        <div>
+            <label class="form-group" for="railRefundType">Select if this is a Domestic or International Rail Refund:</label>
+            <select class="form-control" id="railRefundType" required>
+                <option value="" selected disabled>Please Select</option>
+                <option value="Domestic Rail Refund">Domestic Rail Refund</option>
+                <option value="International Rail Refund">International Rail Refund</option>
+            </select>
+        </div>
+        `
+    } else if (this.value === 'Ancillary') {
+        reasonForRefundField.innerHTML = `
+            <div>
+                <label class="form-group" for="reasonForRefund">Reason for Refund:</label>
+                <select class="form-control" id="reasonForRefund" required>
+                    <option value="" selected disabled>Please Select</option>
+                    <option value="Client's request - Change of plans">Client's request - Change of plans</option>
+                    <option value="Agent Error">Agent Error</option>
+                    <option value="Client Error">Client Error</option>
+                    <option value="Official government travel advice">Official government travel advice</option>
+                    <option value="Supplier issue">Supplier issue</option>
+                </select>
+            </div>`;
+
+        supplierAncField.innerHTML = `
+        <div>
+            <label class="form-group" for="ancillarySupplier">Name of Supplier:</label>
+            <input class="form-control" type="text" id="ancillarySupplier" placeholder="Name of Supplier" required>
+        </div>`;
+    }
+
+
+});
