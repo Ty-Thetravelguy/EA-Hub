01f5f821ddc8a36013ee38ba69cf0fea3bfae30c
Tyrone <145229174+Ty-Thetravelguy@users.noreply.github.com>
2024-03-24 15:48:04 +0000

    finished flight refund form and added the ability to include more than one ticket

--- assets/js/refundForm.js
+++ assets/js/refundForm.js
@@ -27,6 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function refundApplication() {
     document.getElementById('travelTypeRefund').addEventListener('change', function () {
+
+        clearFlightSections();
+
         if (this.value === 'Flight') {
             airRefund();
         } else if (this.value === 'Accommodation') {
@@ -43,9 +46,9 @@ function refundApplication() {
     const airRefund = () => {
 
         const airRefundField = document.getElementById('airRefundTypeSection');
-
+        airRefundField.innerHTML = "";
         airRefundField.innerHTML = `
-            <div class="pt-2">
+            <div class="pt-3">
                 <label class="form-group" for="airRefundType">Select refund type for Flight:</label>
                     <select class="form-control" id="airRefundType" required>
                         <option value="" selected disabled>Please Select</option>
@@ -66,7 +69,7 @@ function refundApplication() {
 
             if (this.value === 'Void') {
                 airReasonForVoidSection.innerHTML = `
-                        <div class="pt-2">
+                        <div class="pt-3">
                             <label class="form-group" for="airReasonForVoid">Reason for Void:</label>
                             <select class="form-control" id="airReasonForVoid" required>
                                 <option value="" selected disabled>Please Select</option>
@@ -80,7 +83,7 @@ function refundApplication() {
 
             } else {
                 airReasonForRefundSection.innerHTML = `
-                        <div class="pt-2">
+                        <div class="pt-3">
                             <label class="form-group" for="airReasonForRefund">Reason for Refund:</label>
                                 <select class="form-control" id="airReasonForRefund" required>
                                     <option value="" selected disabled>Please Select</option>
@@ -97,8 +100,10 @@ function refundApplication() {
         });
 
         const flightNDCSectionQuestion = () => {
-            document.getElementById('flightNDCSection').innerHTML = `
-            <div class="pt-2">
+            const flightNDCSection = document.getElementById('flightNDCSection');
+            flightNDCSection.innerHTML = "";
+            flightNDCSection.innerHTML = `
+            <div class="pt-3">
                 <label class="form-group" for="flightNDC">Is this an NDC flight?</label>
                 <select class="form-control" id="flightNDC" required>
                     <option value="" selected disabled>Please Select</option>
@@ -113,7 +118,7 @@ function refundApplication() {
                 processedAutomaticallySection.innerHTML = "";
                 if (this.value === "Yes") {
                     processedAutomaticallySection.innerHTML = `
-                    <div class="pt-2">
+                    <div class="pt-3">
                         <label class="form-group" for="processedAutomatically">Was the void or refund processed automatically or processed by the airline?</label>
                         <select class="form-control" id="processedAutomatically" required>
                             <option value="" selected disabled>Please Select</option>
@@ -128,7 +133,7 @@ function refundApplication() {
                         bookingSystemSection.innerHTML = "";
                         if (this.value === "No") {
                             bookingSystemSection.innerHTML = `
-                            <div class="pt-2">
+                            <div class="pt-3">
                                 <label class="form-group" for="bookingSystem">Was this booked in Amadeus or Atriis?</label>
                                 <select class="form-control" id="bookingSystem" required>
                                     <option value="" selected disabled>Please Select</option>
@@ -153,8 +158,9 @@ function refundApplication() {
 
         function ndcRef() {
             const ndcRefSection = document.getElementById('ndcSupplierRefSection');
+            ndcRefSection.innerHTML = "";
             ndcRefSection.innerHTML = `
-            <div class="pt-2">
+            <div class="pt-3">
                 <label class="form-group" for="ndcSupplierReference">Supplier Reference</label>
                 <input class="form-control" type="text" id="ndcSupplierReference"
                     placeholder="Airline reference, else if Atriis, Supplier PNR" required>
@@ -163,8 +169,10 @@ function refundApplication() {
         };
 
         function nonNdcSuppliers() {
-            airSupplierSection.innerHTML = `
-            <div class="pt-2">
+            const airSupplierSectionSelect = document.getElementById('airSupplierSection');
+            airSupplierSectionSelect.innerHTML = "";
+            airSupplierSectionSelect.innerHTML = `
+            <div class="pt-3">
                 <label class="form-group" for="airSupplier">Select the supplier:</label>
                 <select class="form-control" id="airSupplier" required>
                     <option value="" selected disabled>Please Select</option>
@@ -197,82 +205,121 @@ function refundApplication() {
 
                 const airSupplierInput = document.getElementById('airSupplierRef');
                 airSupplierInput.innerHTML = `
-                <div class="pt-2">
+                <div class="pt-3">
                     <label class="form-group input-wrapper" for="airSupplerRef">Supplier Reference</label>
                     <input class="form-control" type="text" id="airSupplerRef" placeholder="${placeholderText}" required>
                 </div>`;
 
                 airRefundDataInput();
-
-                // Determine whether to show or hide the Ticket Number input section
-                const ticketNumberSection = document.getElementById('ticketNumberSection'); // Ensure you have this ID on your Ticket Number section
-                if (selectValue === "Travelfusion") {
-                    ticketNumberSection.style.display = "none"; // Hide Ticket Number section
-                } else {
-                    ticketNumberSection.style.display = "block"; // Show Ticket Number section
-                };
             });
         };
 
         function airRefundDataInput() {
-            airRefundData.innerHTML = `
-            
-            <div id="ticketNumberSection" class="pt-2">
-                <label class="form-group" for="airTicketNo">Enter the ticket number</label>
-                <input class="form-control" type="text" id="airTicketNo"
-                    placeholder="EG. 125-1234567890" required>
-            </div>
-            <div class="pt-2">
-                <label class="form-group" for="localCurrenyRefundAmount">Local currency refund amount if applicable:</label>
-                <input class="form-control" type="text" id="localCurrenyRefundAmount"
-                    placeholder="Enter if known.">
-            </div>
-            <div class="pt-2">
-                <label class="form-group" for="refundAmountGBP">Refund amount due to EA in GBP</label>
-                <input class="form-control" type="text" id="refundAmountGBP"
-                    placeholder="Enter the amount we will be receving in GBP." required>
-            </div>
-            <div class="pt-3">
-                <label class="form-group">Is the refund going back to the client?</label>
-                <div>
-                    <input type="radio" id="refundBackToClientYes" name="refundBackToClient" value="Yes" required>
-                    <label for="refundBackToClientYes">Yes</label>
-                    <input type="radio" id="refundBackToClientNo" name="refundBackToClient" value="No" required>
-                    <label for="refundBackToClientNo">No</label>
+            airRefundData.innerHTML = "";
+
+            // Generate HTML for the ticket number input fields based on the number of travellers
+            const numberOfPeopleRefund = document.getElementById('numberOfPeopleRefund').value;
+            let ticketNumberHTML = '';
+            for (let i = 0; i < numberOfPeopleRefund; i++) {
+                ticketNumberHTML += `
+                    <div id="ticketNumberSection${i + 1}" class="pt-3">
+                        <label class="form-group" for="nameAirTicket${i + 1}">Enter traveller ${i + 1}'s name and ticket number</label>
+                        <input class="form-control" type="text" id="nameAirTicket${i + 1}" placeholder="Name of traveller ${i + 1}" required>
+                        <input class="form-control mt-2" type="text" id="airTicketNo${i + 1}" placeholder="EG. 125-1234567890" required>
+                    </div>
+                `;
+            }
+
+            // Generate HTML for other input fields
+            const otherInputsHTML = `
+                <div class="pt-3">
+                    <label class="form-group" for="localCurrenyRefundAmount">Local currency refund amount if applicable:</label>
+                    <input class="form-control" type="text" id="localCurrenyRefundAmount" placeholder="Enter if known.">
                 </div>
-            </div>
-            <div class="pt-2">
-                <label class="form-group">Does your markup need to be refunded?</label>
-                <div>
-                    <input type="radio" id="markUpBackToClientYes" name="markUpBackToClient" value="Yes" required>
-                    <label for="markUpBackToClientYes">Yes</label>
-                    <input type="radio" id="markUpBackToClientNo" name="markUpBackToClient" value="No" required>
-                    <label for="markUpBackToClientNo">No</label>
+                <div class="pt-3">
+                    <label class="form-group" for="refundAmountGBP">Refund amount due to EA in GBP</label>
+                    <input class="form-control" type="text" id="refundAmountGBP" placeholder="Enter the amount we will be receiving in GBP." required>
                 </div>
-            </div>
-            <div class="pt-3">
-                <label class="form-group" for="airNeedToKnow">Is there anything else we need to know?</label>
-                <textarea class="form-control" name="airNeedToKnow" id="airNeedToKnow" cols="80" rows="10" placeholder="Waiver codes, details of the flight which has been cancelled by the airlines for which you need a refund, or the amount you want to keep but are still giving your client something back"></textarea>
-            </div>
-            <div class="row justify-content-between pt-2">
-                <div class="col-2">
-                    <input type="submit" class="btn btn-success btn-lg" value="Submit">
+                <div class="pt-4">
+                    <label class="form-group">Is the refund going back to the client?</label>
+                    <div>
+                        <input type="radio" id="refundBackToClientYes" name="refundBackToClient" value="Yes" required>
+                        <label for="refundBackToClientYes">Yes</label>
+                        <input type="radio" id="refundBackToClientNo" name="refundBackToClient" value="No" required>
+                        <label for="refundBackToClientNo">No</label>
+                    </div>
                 </div>
-                <div class="col-2">
-                    <input type="reset" id="resetButton" class="btn btn-danger btn-lg" value="Reset">
+                <div class="pt-3">
+                    <label class="form-group">Does your markup need to be refunded?</label>
+                    <div>
+                        <input type="radio" id="markUpBackToClientYes" name="markUpBackToClient" value="Yes" required>
+                        <label for="markUpBackToClientYes">Yes</label>
+                        <input type="radio" id="markUpBackToClientNo" name="markUpBackToClient" value="No" required>
+                        <label for="markUpBackToClientNo">No</label>
+                    </div>
                 </div>
-            </div>`;
+                <div class="pt-3">
+                    <label class="form-group" for="airNeedToKnow">Is there anything else we need to know?</label>
+                    <textarea class="form-control" name="airNeedToKnow" id="airNeedToKnow" cols="80" rows="10" placeholder="Waiver codes, details of the flight which has been cancelled by the airlines for which you need a refund, or the amount you want to keep but are still giving your client something back"></textarea>
+                </div>
+                <div class="row justify-content-between pt-3">
+                    <div class="col-2">
+                        <input type="submit" class="btn btn-success btn-lg" value="Submit">
+                    </div>
+                    <div class="col-2">
+                        <input type="reset" id="resetButton" class="btn btn-danger btn-lg" value="Reset">
+                    </div>
+                </div>
+            `;
+
+            // Combine all HTML and set it to airRefundData
+            airRefundData.innerHTML = ticketNumberHTML + otherInputsHTML;
+
+            // Add ticket number validation for each dynamically generated input field
+            for (let i = 0; i < numberOfPeopleRefund; i++) {
+                const airTicketInput = document.getElementById(`airTicketNo${i + 1}`);
+                const ticketNumberPattern = /^\d{3}-\d{10}$/;
+
+                airTicketInput.addEventListener('input', function () {
+                    const inputValue = this.value;
+                    // Check if the input matches the pattern
+                    if (ticketNumberPattern.test(inputValue)) {
+                        // Valid ticket number
+                        this.setCustomValidity('');
+                    } else {
+                        // Invalid ticket number
+                        this.setCustomValidity('Please enter a valid ticket number (e.g., 125-1234567890)');
+                    }
+                });
+            }
+
+            // Reset button functionality
             document.getElementById('resetButton').addEventListener('click', function () {
                 // Reloads the current page
                 window.location.reload();
             });
-
-        };
+        }
 
     };
 
     const accommodationRefund = () => {
-
+        const supplierAccomField = document.getElementById('supplierAccom');
+        supplierAccomField.innerHTML = "";
+        supplierAccomField.innerHTML = `
+        <div class="pt-3">
+            <label class="form-group" for="accomSuppliers">Select the supplier:</label>
+            <select class="form-control" id="accomSuppliers" required>
+                <option value="" selected disabled>Please Select</option>
+                <option value="GDS Hotel">GDS Hotel</option>
+                <option value="AVH">AVH - Amadeus Value Hotels</option>
+                <option value="Expedia TAAP">Expedia TAAP</option>
+                <option value="Expedia EAN">Expedia EAN</option>
+                <option value="Booking.com via Amadeus">Booking.com via Amadeus</option>
+                <option value="Booking.com via Atriis">Booking.com via Atriis</option>
+                <option value="Hotel Website">Hotel Website</option>
+            </select>
+        </div>
+        `;
     };
 
     const carHireAndTaxiRefund = () => {
@@ -283,6 +330,10 @@ function refundApplication() {
 
     const ancillaryRefund = () => {
     };
+
+    function landRefundData() {
+
+    }
 
 };
 
@@ -308,4 +359,35 @@ function refundApplication() {
 // });
 
 
+function clearFlightSections() {
+    // Clear all sections related to flights
+    const airRefundField = document.getElementById('airRefundTypeSection');
+    airRefundField.innerHTML = '';
 
+    const airReasonForVoidSection = document.getElementById('airReasonForVoidSection');
+    airReasonForVoidSection.innerHTML = '';
+
+    const airReasonForRefundSection = document.getElementById('airReasonForRefundSection');
+    airReasonForRefundSection.innerHTML = '';
+
+    const flightNDCSection = document.getElementById('flightNDCSection');
+    flightNDCSection.innerHTML = '';
+
+    const processedAutomaticallySection = document.getElementById('processedAutomaticallySection');
+    processedAutomaticallySection.innerHTML = '';
+
+    const bookingSystemSection = document.getElementById('bookingSystemSection');
+    bookingSystemSection.innerHTML = '';
+
+    const ndcRefSection = document.getElementById('ndcSupplierRefSection');
+    ndcRefSection.innerHTML = '';
+
+    const airSupplierSectionSelect = document.getElementById('airSupplierSection');
+    airSupplierSectionSelect.innerHTML = '';
+
+    const airSupplierInput = document.getElementById('airSupplierRef');
+    airSupplierInput.innerHTML = '';
+
+    const airRefundData = document.getElementById('airRefundData');
+    airRefundData.innerHTML = '';
+}
